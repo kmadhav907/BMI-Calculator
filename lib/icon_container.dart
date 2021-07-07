663d850bb74fe41807e8570be1b7e0cde83240d7
kmadhav907 <kmadhav907@gmail.com>
2021-07-07 09:23:26 +0530

    Basic func added

---
+++ lib/icon_container.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class IconContainer extends StatelessWidget {
+  final IconData icon;
+  final String label;
+  IconContainer({@required this.icon, @required this.label});
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          icon,
+          size: 80.0,
+        ),
+        SizedBox(
+          height: 15.0,
+        ),
+        Text(
+          label,
+          style: TextStyle(fontSize: 18.0, color: Color(0xFF8D8E98)),
+        )
+      ],
+    );
+  }
+}
--- lib/reused_card.dart
+++ lib/reused_card.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 
 class ReusedContainer extends StatelessWidget {
   final Color colour;
-  ReusedContainer({@required this.colour});
+  final Widget cardChild;
+  ReusedContainer({@required this.colour, this.cardChild});
   @override
   Widget build(BuildContext context) {
     return Container(
+        child: cardChild,
         margin: EdgeInsets.all(15.0),
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(10.0),
